d0d52ec5f2d06e4c6191895396cd6b49bf429773
Mike <mikededecker@live.nl>
2020-02-02 16:17:29 +0100

    Change to javascript

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const exec = require('@actions/exec');
 
 try {
     // `who-to-greet` input defined in action metadata file
@@ -7,9 +8,7 @@ try {
     console.log(`Hello ${nameToGreet}!`);
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+    await exec.exec('"php -v"',);
 } catch (error) {
     core.setFailed(error.message);
 }
